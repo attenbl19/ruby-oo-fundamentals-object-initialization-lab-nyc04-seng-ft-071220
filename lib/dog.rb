b1c34b4adcf62fd9792bf8c03d6d3cc942c1b30a
Lida Attenborough <attenboroughl@protonmail.com>
2020-08-03 20:38:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,7 @@
 class Dog
-  def initialize(name, breed, no_breed = "Mutt")
+  def initialize(name, breed = "Mutt")
      @name = name
      @breed = breed
      @no_breed = "Mutt"
-  end
- 
-  def breed=(breed, name, no_breed)
-    @breed = breed
-    @name = name
-    @no_breed = "Mutt"
-  end
- 
-  def breed
-    @breed
   end
 end
